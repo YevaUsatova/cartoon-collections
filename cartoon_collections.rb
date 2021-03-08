37857150285d2ed065841e2bbd363fbece1b7a53
YevaUsatova <yeva.usatova@gmail.com>
2021-03-08 01:55:02 -0500

    commit tree 4898e7ee20c14d40f77d881c9e652472ff35147d

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,28 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.each_with_index do |name,index|
+    puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planets)# code an argument here
   # Your code here
+  planets.collect! do |planet| 
+    planet.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(call)# code an argument here
   # Your code here
+  word = call.length
+  call.each_with_index do |words, i|
+    if words[i] > 4
+      true
+    else
+      false
+    end
+  end
+  word
 end
 
 def find_the_cheese# code an argument here
